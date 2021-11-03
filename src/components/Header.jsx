6ed1ac7c60052a3625146454e8f520f5c404fc23
Joshman <ejembijoshman@gmail.com>
2021-11-03 15:05:30 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-logo"><span>D</span>ev-<span>G</span>uy</div>
+            <div className="contact">
+                <a href="mailto:ejembijoshman@gmail.com">Contact</a>
+            </div>
+        </header>
+    );
+}
+ 
+export default Header;
